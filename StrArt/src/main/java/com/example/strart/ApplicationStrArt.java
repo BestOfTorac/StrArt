1c66c7b8a20d90b6be31aebd39df35d529e62365
BestOfTorac <toracvale@gmail.com>
2024-01-02 19:25:56 +0100

    valeeee

--- StrArt/src/main/java/com/example/strart/ApplicationStrArt.java
+++ StrArt/src/main/java/com/example/strart/ApplicationStrArt.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Application_StrArt extends Application {
+public class ApplicationStrArt extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application_StrArt.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationStrArt.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 414, 796);
         stage.setTitle("Hello!");
         stage.setScene(scene);
