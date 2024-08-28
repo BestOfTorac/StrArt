b823e26966f1a0934b61a032be865b78d82dbb1b
BestOfTorac <toracvale@gmail.com>
2024-08-28 13:19:32 +0200

    risoluzione di alcuni code smells

--- StrArt/src/main/java/com/strart/model/dao/VisualizzaEventiDAO.java
+++ StrArt/src/main/java/com/strart/model/dao/VisualizzaEventiDAO.java
@@ -30,20 +30,20 @@ public class VisualizzaEventiDAO implements GenericDAO<ListEvento> {
                 while (rs.next()) {
                     Evento evento= factoryEvento.createEvento(rs.getString(8));
                     evento.setDescrizione(rs.getString(1));
+                    evento.setImmagine(rs.getBlob(7));
                     evento.setLatitudine(rs.getString(2));
                     evento.setLongitudine(rs.getString(3));
                     evento.setData(rs.getDate(4));
                     evento.setOrarioInizio(rs.getTime(5));
                     evento.setOrarioFine(rs.getTime(6));
-                    evento.setImmagine(rs.getBlob(7));
                     evento.setStato(rs.getString(9));
-
 
                     listEvento.addEvento(evento);
                 }
             }
 
         } catch (SQLException e) {
+            //lancio un messaggio di errore
             throw new DAOException("Listino eventi in VisualizzaEventiDAO error: " + e.getMessage());
         }finally {
             if(cs!= null){
