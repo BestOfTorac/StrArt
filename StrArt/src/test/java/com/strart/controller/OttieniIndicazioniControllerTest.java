74216a40d1b6b484dc4b45062ae6e90f792f002d
BestOfTorac <toracvale@gmail.com>
2024-08-28 13:05:14 +0200

    inserimento driver della connessione inclusa nel maven e risoluzione di alcuni code smells

---
+++ StrArt/src/test/java/com/strart/controller/OttieniIndicazioniControllerTest.java
@@ -0,0 +1,53 @@
+package com.strart.controller;
+
+import com.strart.model.bean.BeanEventi;
+import com.strart.model.bean.IndirizzoBean;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OttieniIndicazioniControllerTest {
+
+    OttieniIndicazioniController indicazioni = null;
+
+    @BeforeAll
+    void setup() {
+        indicazioni = new OttieniIndicazioniController();
+    }
+
+    @Test
+    void cercaEventiSuccessfull() {
+        //TODO metti nel db una data lontana (2025)
+        IndirizzoBean indirizzoB = new IndirizzoBean("via dei pini 00013");
+        BeanEventi eventi = null;
+
+        try {
+            eventi = indicazioni.cercaEventi(indirizzoB);
+        } catch (Exception e) {
+            fail();
+        }
+
+        assertNotEquals(null, eventi);
+    }
+
+
+    @Test
+    void cercaEventiAddressNotFound() {
+
+        IndirizzoBean indirizzoB = new IndirizzoBean("address not found");
+        BeanEventi eventi = null;
+
+        try {
+            eventi = indicazioni.cercaEventi(indirizzoB);
+        } catch(IllegalArgumentException e) {
+            assertEquals("L'indirizzo inserito non è valido", e.getMessage());
+        } catch (Exception e) {
+            fail();
+        }
+
+        assertEquals(null, eventi);
+    }
+}
